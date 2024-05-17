8580ec2599e99be716561f0dced1aa012fe29cf2
egor-udalov <zoraudalov@gmail.com>
2024-05-17 12:32:39 +0300

    feat: migrations, endpoints: new items, hits

--- shop/components/templates/MainPage/MainPage.tsx
+++ shop/components/templates/MainPage/MainPage.tsx
@@ -1,11 +1,17 @@
 import Hero from '@/components/modules/MainPage/Hero/Hero'
 import Categories from '@/components/modules/MainPage/Categories/Categories'
+import { useGate } from 'effector-react'
+import { MainPageGate } from '@/context/goods'
 
-const MainPage = () => (
-  <main>
-    <Hero />
-    <Categories />
-  </main>
-)
+const MainPage = () => {
+  useGate(MainPageGate)
+
+  return (
+    <main>
+      <Hero />
+      <Categories />
+    </main>
+  )
+}
 
 export default MainPage
